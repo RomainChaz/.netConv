049e32a336612a6d6ddf220b4fa43308bb4fb66c
Rominou <romainchazottier@gmail.com>
2017-09-28 13:51:34 +0200

    first commit

---
+++ ClientConvertisseurV1/Model/Devise.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WSConvertisseur.Models
+{
+    public class Devise
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public double Taux { get; set; }
+
+        public Devise (int id, string nom, double taux)
+        {
+            this.Id = id;
+            this.Nom = nom;
+            this.Taux = taux;
+        }
+
+        public Devise()
+        {
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+    }
+}
